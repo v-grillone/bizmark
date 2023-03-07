230398aa4fea4bc14e801236bb746470d4f97bde
Vincenzo Grillone <vincenzo_1996@hotmail.com>
2023-03-06 19:42:38 -0500

    search container open and close functionality

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+// Search icon click open
+
+const searchIcon = document.querySelector('.search-icon')
+
+const toogleSearch = (e) => {
+  const searchContainer = document.querySelector('.search-container');
+  const searchBox = document.querySelector('.search-box')
+  if(Array.from(searchContainer.classList).includes('closed')){
+    searchContainer.style.width = '28vw';
+    searchContainer.classList.remove('closed');
+    // searchBox.style.width = '25px'
+  }else{
+    searchContainer.style.width = '7vw';
+    searchContainer.classList.add('closed');
+  }
+}
+
+searchIcon.addEventListener('click', toogleSearch)
