7e2b105371a578926e70e7987ccd92e01ac7370e
Vincenzo Grillone <vincenzo_1996@hotmail.com>
2023-03-12 18:19:06 -0400

    rough main layout

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+// Fake API
+locations = [
+  {
+    id: 1,
+    img:'./assets/paris-background.jpg',
+    country: 'France',
+    city: 'Paris',
+    description: 'France ipsum dolor sit amet consectetur adipisicing elit. Dicta natus esse error ratione mollitia exercitationem ducimus. Quam amet similique nisi, ipsam libero illum consequuntur repellendus ut placeat, iste optio perferendis dignissimos quas facere voluptas facilis. Voluptatum quas, quae exercitationem maxime voluptatem non commodi. Qui eius modi iusto, quam quae accusamus eveniet adipisci iure sed odio ducimus suscipit repellendus minima fuga natus, totam delectus. Similique.',
+    flightCost: '100$',
+    hotelCost: '750$ / 7 Days'
+  },
+  {
+    id: 2,
+    img:'./assets/rome-background.jpg',
+    country: 'Italy',
+    city: 'Rome',
+    description: 'Italy ipsum dolor sit amet consectetur adipisicing elit. Dicta natus esse error ratione mollitia exercitationem ducimus. Quam amet similique nisi, ipsam libero illum consequuntur repellendus ut placeat, iste optio perferendis dignissimos quas facere voluptas facilis. Voluptatum quas, quae exercitationem maxime voluptatem non commodi. Qui eius modi iusto, quam quae accusamus eveniet adipisci iure sed odio ducimus suscipit repellendus minima fuga natus, totam delectus. Similique. fuga natus, totam delectus. Similique.',
+    flightCost: '210$',
+    hotelCost: '600$ / 7 Days'
+  },
+  {
+    id: 3,
+    img:'./assets/tokyo-background.jpg',
+    country: 'Japan',
+    city: 'Tokyo',
+    description: 'Japan ipsum dolor sit amet consectetur adipisicing elit. Dicta natus esse error ratione mollitia exercitationem ducimus. Quam amet similique nisi, ipsam libero illum consequuntur repellendus ut placeat, iste optio perferendis dignissimos quas facere voluptas facilis. Voluptatum quas, quae exercitationem maxime voluptatem non commodi. Qui eius modi iusto, quam quae accusamus eveniet adipisci iure sed odio ducimus suscipit repellendus minima fuga natus, totam delectus. Similique.',
+    flightCost: '270$',
+    hotelCost: '900$ / 7 Days'
+  },
+]
+
+
 // Search icon click open
 
 const searchIcon = document.querySelector('.search-icon')
